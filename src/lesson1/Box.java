8d82ed57b523039454df7bed025a270c71151ae8
Alexander <tehalexpetrov@gmail.com>
2021-02-08 06:40:29 +0300

    первое задание 3 уровня

--- src/lesson1/Box.java
+++ src/lesson1/Box.java
@@ -4,10 +4,36 @@ import java.util.ArrayList;
 
 public class Box <T extends Fruit>{
     private ArrayList<T> box = new ArrayList<>();
+    private T fruit;
 
     public Box() {
-        public float getWeight(){
 
+    }
+
+    public float getWeiht(){
+        float weight = 0.0f;
+
+        for (T t : box) {
+           weight += t.getWeight();
+        }
+        return weight;
+    }
+
+    public boolean compare(Box anotherBox) {
+        if (getWeiht() == anotherBox.getWeiht()){
+            return true;
+        }
+        return false;
+    }
+
+    public void pourTo(Box <T>anotherBox){
+        anotherBox.box.addAll(box);
+        box.clear();
+    }
+
+    public void addFruit(T fruit, int amount) {
+        for (int i = 0; i < amount; i++) {
+            box.add(fruit);
         }
     }
 }
--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -3,6 +3,7 @@ package lesson1;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,9 +23,24 @@ public class Main {
         System.out.println("*********************");
 
         //Третье задание
+        Box<Apple> ap = new Box<>();
+        Box<Apple> ap1 = new Box<>();
+        Box<Orange> or = new Box<>();
+        Box<Orange> or1 = new Box<>();
 
+        ap.addFruit(new Apple(),10);
+        ap1.addFruit(new Apple(),8);
+        or.addFruit(new Orange(), 7);
+        or1.addFruit(new Orange(), 4);
 
+        equalBox(ap, ap1, or, or1); //Сравним коробки
+
+        or.pourTo(or1);
+        ap.pourTo(ap1);
+
+        weightBox(ap, ap1, or, or1);
     }
+
 
     public static void arrayBack(Object[] arr){  //Пишем метод, который будет менять первых два элемента массива местами
         System.out.println("\nРезультат работы метода");
@@ -37,5 +53,17 @@ public class Main {
     public static  <T> void arrList(T[] arr){
         ArrayList<T> arrConvert = new ArrayList<>(Arrays.asList(arr));
         System.out.println(arrConvert);
+    }
+
+    private static void equalBox(Box<Apple> ap, Box<Apple> ap1, Box<Orange> or, Box<Orange> or1) {
+        System.out.println("Box 1 equals box 3: "+ or.compare(ap));
+        System.out.println("Box 2 equals box 4: "+ or1.compare(ap1));
+    }
+
+    private static void weightBox(Box<Apple> ap, Box<Apple> ap1, Box<Orange> or, Box<Orange> or1) {
+        System.out.println("Первая коробка с апельсинами: "+ or.getWeiht());
+        System.out.println("Вторая коробка с апельсинами: "+ or1.getWeiht());
+        System.out.println("Первая коробка с яблоками: "+ ap.getWeiht());
+        System.out.println("Вторая коробка с яблоками: "+ ap1.getWeiht());
     }
 }
